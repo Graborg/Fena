41687285025fab63b46ee0ec8189a28d4e8c5345
graborg <intemicke@gmail.com>
2013-11-03 22:41:03 +0100

    Mergeconflict

--- src/com/example/fena/JsonPersonreceiver.java
+++ src/com/example/fena/JsonPersonreceiver.java
@@ -20,25 +20,15 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
-<<<<<<< HEAD
-public abstract class JsonPersonreceiver extends
-		AsyncTask<String, String, ArrayList<Person>> implements
-		CallbackReceiver {
-=======
 public abstract class JsonPersonreceiver extends AsyncTask<String, String, ArrayList<Person>> implements CallbackReceiver {
->>>>>>> f8444c16c1b2dbc19b91528c88c7cab557731f62
 	private ProgressDialog mProgressDialog;
 	Handler handler;
 	Runnable callback;
 	Activity activity;
-<<<<<<< HEAD
 
-	public JsonPersonreceiver(Activity activity) {
-=======
 	
 	
 	public JsonPersonreceiver(Activity activity){
->>>>>>> f8444c16c1b2dbc19b91528c88c7cab557731f62
 		this.activity = activity;
 		mProgressDialog = new ProgressDialog(activity);
 		mProgressDialog.setMessage("Loading Please Wait.");
@@ -47,29 +37,15 @@ public abstract class JsonPersonreceiver extends AsyncTask<String, String, Array
 		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
 		mProgressDialog.setCancelable(true);
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> f8444c16c1b2dbc19b91528c88c7cab557731f62
 	protected void onPreExecute() {
 		mProgressDialog = ProgressDialog.show(activity, "", "Please Wait",
 				true, false);
 		super.onPreExecute();
 	}
-<<<<<<< HEAD
-
-	@Override
-	protected ArrayList<Person> doInBackground(String... url) {
-		// InputStream source = retrieveStream(url);
-		// Reader reader = new InputStreamReader(source);
-
-=======
 	@Override
 	protected ArrayList<Person> doInBackground(String... url) {
 		//InputStream source = retrieveStream(url);
 		//Reader reader = new InputStreamReader(source);
-		
->>>>>>> f8444c16c1b2dbc19b91528c88c7cab557731f62
 		Gson gson = new Gson();
 		String s = "{\"1\":[{\"name\":\"Micke\",\"expectations\":\"\",\"skills\":\"\",\"description\":\"\",\"experience\":\"\"}],\"2\":[{\"name\":\"Micke\",\"expectations\":\"\",\"skills\":\"\",\"description\":\"\",\"experience\":\"\"}],\"3\":[{\"name\":\"Micke\",\"expectations\":\"None\",\"skills\":\"Many\",\"description\":\"Yes I want to want\",\"experience\":\"From everywhere\"}]}";
 		Reader reader = new StringReader(s);
@@ -77,7 +53,6 @@ public abstract class JsonPersonreceiver extends AsyncTask<String, String, Array
 		return results.persons;
 	}
 
-<<<<<<< HEAD
 	protected void onPostExecute(ArrayList<Person> persons) {
 		if (mProgressDialog != null || mProgressDialog.isShowing()) {
 			mProgressDialog.dismiss();
@@ -87,10 +62,6 @@ public abstract class JsonPersonreceiver extends AsyncTask<String, String, Array
 		}
 	}
 
-	public abstract void receiveData(Object object);
-
-=======
->>>>>>> f8444c16c1b2dbc19b91528c88c7cab557731f62
 	private InputStream retrieveStream(String url) {
 
 		DefaultHttpClient client = new DefaultHttpClient();
@@ -119,8 +90,6 @@ public abstract class JsonPersonreceiver extends AsyncTask<String, String, Array
 		return null;
 
 	}
-<<<<<<< HEAD
-=======
 	
 	protected void onPostExecute(String jsondata) {
 		if (mProgressDialog != null || mProgressDialog.isShowing()) {
@@ -133,5 +102,4 @@ public abstract class JsonPersonreceiver extends AsyncTask<String, String, Array
 
 	public abstract void receiveData(Object object);
 
->>>>>>> f8444c16c1b2dbc19b91528c88c7cab557731f62
 }
